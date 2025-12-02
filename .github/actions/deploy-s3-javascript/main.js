93893c32180221e7f57f402965b0d1a411785dec
Aman Ishaq <aman-ishaq@hotmail.com>
2025-12-02 16:41:43 +0000

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,7 +12,9 @@ function run() {
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
     
-    core.notice('Hello, from custom action');
+    // 3). Generate URL
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl); // echo "name=value" >> $GITHUB_OUTPUT
 }
 
 run();
